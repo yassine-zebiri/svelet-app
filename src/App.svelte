4d093f55039012621436339d31f4c6cbe5b8c452
YASSINE ZEBIRI <yassinezebiri07@gmail.com>
2022-07-01 23:57:49 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+
+<!--   javascript  -->
+<script>
+import Counter from "./lib/Counter.svelte";
+
+    
+</script>
+
+
+<!--   TAG HTML  -->
+<svelte:head>
+	<link rel="stylesheet" href="/src/css/bootstrap-5.css">
+</svelte:head>
+<main class="container-fluid p-1 my-contant">
+    <header class="mb-5 header">SVELTE APP</header>
+    <div class="mt-5 ">
+        
+        <Counter/>
+    </div>
+</main>
+
+
+<!--   CSS style  -->
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+.my-contant{
+display: flex;
+justify-content: flex-start;
+align-items: center;
+flex-direction: column;
+border: 1px black solid;
+margin: auto;
+width: 100%;
+height: 150vh;
+background: linear-gradient(50deg,#03001e,#7303c0,#ec38bc,#fdeff9);
+}
+.header{
+    position: fixed;
+    height: 70px;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(8px);
+    display: flex;
+    align-items: center;
+    padding: 0 2rem;
+    font-size: 18px;
+    font-weight: 600;
+    top: 0%;
+    color: white;
+}
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,28 @@
+<script>
+  let value=0;
+  const inc=(nbr)=>{
+    value+=nbr;
+  }
+  const rest=()=>{
+    value=0;
+  }
+  
+</script>
+
+<main class="mt-5 w-100">
+  <h1 class="text-white" >counter : {value}</h1>
+  <div class="group-btn p-1 pt-5">
+  <button on:click={()=>inc(1)} class="btn btn-primary">+</button>
+  <button on:click={()=>rest()} class="btn btn-secondary">rest</button>
+  <button on:click={()=>inc(-1)} class="btn btn-danger">-</button>
+  </div>
+</main>
+
+
+<style>
+  .group-btn{
+    display: flex;
+    justify-content: space-between;
+    
+  }
+</style>
